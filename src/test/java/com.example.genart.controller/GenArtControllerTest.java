5978c1b9fcd899f6c703fa9abc5282e13914de00
izlatkin <zlatkin_ilya@mail.ru>
2025-06-27 22:27:16 -0400

    test update

--- src/test/java/com.example.genart.controller/GenArtControllerTest.java
+++ src/test/java/com.example.genart.controller/GenArtControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,6 +20,7 @@ class GenArtControllerTest {
     @Test
     void helloEndpointReturnsOk() throws Exception {
         mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, GenArt!"));
     }
 }
